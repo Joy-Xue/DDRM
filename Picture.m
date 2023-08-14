828b76d336a1cdf7aa065aa838012e9d5d01191b
Joy-Xue <45956613+Joy-Xue@users.noreply.github.com>
2023-08-14 16:16:53 +0800

    rename Copy_of_picture.m to Picture.m

--- Picture.m
+++ Picture.m
@@ -1,10 +1,10 @@
 
 
-y=[0.4416    0.2941    0.1210    0.0639    0.0386    0.0495    0.0356    0.0337];
+L0=[0.4416    0.2941    0.1210    0.0639    0.0386    0.0495    0.0356    0.0337];
 
 E = [0.2,0.5,1,2,3,4,5,6];
 
-plot(E,y,'->','linewidth',3,'color','g', 'markersize',12, 'markerfacecolor', 'g');
+plot(E,L0,'->','linewidth',3,'color','g', 'markersize',12, 'markerfacecolor', 'g');
 hold on
 grid on
 set(gca,'FontSize',20);
